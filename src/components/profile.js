f44400e38ea2be2923ef837d32b048fd202ce67b
mayo <mayowa.wh@gmail.com>
2022-10-31 13:34:43 +0000

    Reformat id tag in the profile component

--- src/components/profile.js
+++ src/components/profile.js
@@ -8,7 +8,7 @@ export default  function Profile(){
     return(
         <header>
             <div id="header_profile" >
-                <img id="profile_img" src={mayo} alt="side face view in monochrome" />
+                <img id="profile__img" src={mayo} alt="side face view in monochrome" />
                     <p id="twitter" >MayowaWn </p>
                     {/* <p id="slack" >Mayowa Ade</p> */}
 
