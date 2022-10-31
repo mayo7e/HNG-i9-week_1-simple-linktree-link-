80bd58b4f9898a92ed39653f01b7ecce41d0fd17
mayo <mayowa.wh@gmail.com>
2022-10-31 15:49:04 +0000

    Add avatar place hoolder

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,6 +1,8 @@
 
 import React from "react"
 import mayo from "../assets/mayo.jpg"
+import avatar from "../assets/avatar-svgrepo.svg"
+
 // import '../index.css';
 
 
@@ -8,7 +10,14 @@ export default  function Profile(){
     return(
         <header>
             <div id="header_profile" >
-                <img id="profile__img" src={mayo} alt="side face view in monochrome" />
+                
+                    <img id="profile__img" class="image" src={mayo} alt="Avatar" />
+                    <div class="overlay">
+                            <a href="0#" class="icon" title="User Profile">
+                                <img alt="avatar placeholder" src={avatar} id="avatar_placeholder" />
+                            </a>
+
+                </div>
                     <p id="twitter" >MayowaWn </p>
                     {/* <p id="slack" >Mayowa Ade</p> */}
 
