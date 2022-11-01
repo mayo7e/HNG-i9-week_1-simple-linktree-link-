fe4e0c9906a9c35ddad0bdfc009d4a226a90b4b8
mayo7e <100059302+mayo7e@users.noreply.github.com>
2022-11-01 09:54:01 +0100

    changed file path

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+
+import Profile from "../components/profile.js"
+import Main from "../components/main.js"
+import Footer from "../components/footer.js"
+import '../index.css';
+
+function App() {
+  return (
+    <div className="App">
+          <Profile />
+          <Main />
+          <Footer />
+          {/* <Content /> */}
+
+    </div>
+  );
+}
+
+export default App;
