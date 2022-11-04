9bf42f25fb890dc9f6e6a509c16588e3144036f7
mayo7e <mayowa.wh@gmail.com>
2022-11-04 19:57:20 +0000

    Add form functionality in the contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,10 +2,107 @@ import React from "react"
 
 
 
+  const myName = "Mayowa"
 
 export default function Contact (){
+    const [formData, setFormData] = React.useState(
+      {
+          firstName: "", 
+          lastName: "", 
+          email: "",
+          typeMessage: "", 
+          isUseData: true,
+          
+      }
+  )
+
+  function handleChange(event) {
+    const {name, value, type, checked} = event.target
+    setFormData(prevFormData => {
+        return {
+            ...prevFormData,
+            [name]: type === "checkbox" ? checked : value
+        }
+    })
+}
+
+function handleSubmit(event) {
+ 
+  console.log(formData)
+}
+
+
+
+
     return (
-      <>Contact</>
+     <div>
+          <form onSubmit={handleSubmit} >
+                <section>
+                          <label for="first_name" >First name</label>
+                          <input
+                              type="text"
+                              id = "first_name"
+                              placeholder="Enter your first name"
+                              onChange={handleChange}
+                              name="firstName" 
+                              value={formData.firstName}
+                              
+                          />
+                    
+                          <label for="last_name" >Last name</label>
+                          <input
+                              type="text"
+                              id = "last_name"
+                              placeholder="Enter your last name"
+                              onChange={handleChange}
+                              name="lastName"
+                              value={formData.lastName}
+                          />
+                </section>
+                <section>
+                          <label for="email" >Email</label>
+                          <input
+                              type="email"
+                              id = "email" 
+                              placeholder="Enter your Email address" 
+                              onChange={handleChange} 
+                              name="email"  
+                              value={formData.email}
+                          />
+                    
+                </section>
+                <section>
+                          <label for="message" >Message</label>
+                          <textarea
+                              value={formData.message} 
+                              type="text"
+                              id = "message"
+                              placeholder="Send me a message and i'll re[ly ypu as soon as possible"
+                              onChange={handleChange} 
+                              name="typeMessage" 
+                              rows="10"
+                              cols="50"
+                          />
+                    
+                </section>
+                <section>
+                          <input
+                              type="checkbox"
+                              id = "isUsaData" 
+                              onChange={handleChange}  
+                              name="isUseData"
+                              checked={formData.isUseData}
+                          />
+                          <label for="checkbox" >You agree to prividing your data to {myName} who may contact you.</label>
+                    
+                </section>
+
+                <button id="btn__submit" >Send message</button>
+
+
+          </form>
+
+     </div>
       
     )
   }
