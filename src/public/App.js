6dc344415c62fe344bbbf1fccdc6f8eb2b662a46
mayo7e <mayowa.wh@gmail.com>
2022-11-05 14:57:32 +0000

    Refactor code for netlify deployment

--- src/public/App.js
+++ src/public/App.js
@@ -5,21 +5,20 @@ import Contact from "../components/contact.js"
 import Footer from "../components/footer.js"
 import NotFound from "../components/notFound.js"
 
-import { HashRouter as Router, Route, Routes  } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
 import '../index.css';
 
 function App() {
   return (
     <Router 
-    
+      basename="hngi9_week_01-_linktree"
     >
    
             
         <Routes>
             <Route index path="/" element={<Home />} />
-            <Route index path="home" element={<Home />} />
-            <Route path= "contact" element={<Contact />} />
-            <Route path= "*" element={<NotFound />} />
+            <Route path= "/contact" element={<Contact />} />
+            <Route path= "/*" element={<NotFound />} />
 
         </Routes>
           <Footer />
