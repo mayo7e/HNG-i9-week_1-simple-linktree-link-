3edd14083778fa2584472b6d47a79ddfee614f51
mayo7e <mayowa.wh@gmail.com>
2022-11-05 19:51:04 +0000

    Update HashRouter to BrowserRouter

--- src/public/App.js
+++ src/public/App.js
@@ -4,12 +4,12 @@ import Home from "../components/home.js"
 import Contact from "../components/contact.js"
 import NotFound from "../components/notFound.js"
 
-import { HashRouter as Router, Route, Routes  } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
 import '../index.css';
 
 function App() {
   return (
-    <Router >
+    <Router basename="hngi9_week_01-_linktree" >
      
         <Routes>
             <Route index path="" element={<Home />} />
